a95f5f6593e7ae9ed70f8c0827a802596c020886
cyanchill <anthonyliang9@gmail.com>
2021-08-16 14:36:10 -0400

    add ability to add books to the library

--- main.js
+++ main.js
@@ -4,43 +4,70 @@ const addNewBookBtn = document.getElementById("new-book");
 const bookFormScreen = document.getElementById("book-form-screen");
 const addBookForm = document.getElementById("add-book");
 
-let myLibrary = [];
+let myLibrary = JSON.parse(localStorage.getItem("libraryBooks")) || [];
 
-function Book() {
-  title = "";
-  author = "";
-  pages = 0;
-  read = false;
+function Book(title, author, pages, read, bookImg = "") {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.bookImg = bookImg;
 }
-
-Book.prototype.info = function () {
-  return `${this.title} by ${this.author}, ${pages} pages, ${
-    read ? "read" : "not read yet"
-  }`;
-};
 
 function addBookToLibrary() {}
 
-function loadBooks() {}
+function loadBooks() {
+  if (!myLibrary) return;
+  myLibrary.forEach((book) => {
+    addBookToDisplay(book);
+  });
+}
 
-function addBookToDisplay() {}
+function addBookToDisplay(book) {
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("book-card");
+  const bookImgURL = book.bookImg || "missing_cover.jpg";
+  bookCard.innerHTML = `
+    <img src="${bookImgURL}" alt="${book.title} cover image">
+    <div class="book-info">
+      <p class="title">${book.title}</p>
+      <p class="author">${book.author}</p>
+      <p class="pages">${book.pages} pages</p>
+    </div>
+  `;
+  library.appendChild(bookCard);
+}
+
+function removeBook() {}
+
+function updateReadStatus() {}
 
 addNewBookBtn.addEventListener("click", showForm);
-
 // Listen to when form is submitted
 document.addEventListener("submit", submitForm);
 
 function submitForm(e) {
   e.preventDefault();
+  const newBook = new Book(
+    e.target.title.value,
+    e.target.author.value,
+    e.target.pages.value,
+    e.target.read.checked,
+    e.target.bookImg.value
+  );
+  myLibrary.push(newBook);
+  localStorage.setItem("libraryBooks", JSON.stringify(myLibrary));
+  addBookToDisplay(newBook);
   addBookForm.reset();
+  hideForm();
 }
 
 function showForm() {
-  bookFormScreen.classList.add("add-book-form-enter");
+  bookFormScreen.classList.add("form-screen-enter");
   setTimeout(
     () =>
-      bookFormScreen.classList.contains("add-book-form-enter") &&
-      bookFormScreen.classList.add("add-book-form-active"),
+      bookFormScreen.classList.contains("form-screen-enter") &&
+      bookFormScreen.classList.add("form-screen-enter-active"),
     150
   );
   bookFormScreen.classList.add("active");
@@ -55,11 +82,14 @@ function showForm() {
 function hideForm() {
   document.removeEventListener("click", exitForm);
   bookFormScreen.classList.remove(
-    "add-book-form-enter",
-    "add-book-form-active"
+    "form-screen-enter",
+    "form-screen-enter-active"
   );
 }
 
 function exitForm() {
   bookFormScreen.classList.remove("active");
 }
+
+// Initialization:
+loadBooks();
