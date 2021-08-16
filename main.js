4029577309a05c68481aa49bf49d3096b994b1f1
cyanchill <anthonyliang9@gmail.com>
2021-08-15 22:27:47 -0400

    added code for hiding and showing book form on screen

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-const library = document.getElementById('library');
+const body = document.querySelector("body");
+const library = document.getElementById("library");
+const newBookBtn = document.getElementById("new-book");
+const bookFormScreen = document.getElementById("book-form-screen");
+const addBookForm = document.getElementById("add-book");
+
 let myLibrary = [];
 
 function Book() {
@@ -14,14 +19,42 @@ Book.prototype.info = function () {
   }`;
 };
 
-function addBookToLibrary() {
+function addBookToLibrary() {}
 
+function loadBooks() {}
+
+function addBookToDisplay() {}
+
+newBookBtn.addEventListener("click", showForm);
+
+// Listen to when form is submitted
+document.addEventListener("submit", hideForm);
+
+function hideForm(e) {
+  e.preventDefault();
+  document.removeEventListener("click", exitForm);
+  addBookForm.reset();
+  bookFormScreen.classList.remove(
+    "add-book-form-enter",
+    "add-book-form-active"
+  );
 }
 
-function loadBooks() {
-
+function showForm() {
+  bookFormScreen.classList.add("add-book-form-enter");
+  setTimeout(
+    () =>
+      bookFormScreen.classList.contains("add-book-form-enter") &&
+      bookFormScreen.classList.add("add-book-form-active"),
+    150
+  );
+  bookFormScreen.classList.add("active");
+  // Listen to if we click outside the form (to close)
+  document.addEventListener("click", (e) => {
+    if (e.target.dataset.hasOwnProperty("outside")) hideForm(e);
+  });
 }
 
-function addBookToDisplay() {
-
+function exitForm() {
+  bookFormScreen.classList.remove("active");
 }
