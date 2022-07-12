7aa83b5b4962471f74435ac985ac5c7b3c77d4db
cyanChill <anthonyliang9@gmail.com>
2022-07-12 12:31:16 -0400

    add offline support & caching

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,8 +3,9 @@ importScripts(
 );
 
 const { clientsClaim } = workbox.core;
+const { BackgroundSyncPlugin } = workbox.backgroundSync;
 const { ExpirationPlugin } = workbox.expiration;
-const { precacheAndRoute } = workbox.precaching;
+const { precacheAndRoute, createHandlerBoundToURL } = workbox.precaching;
 const { registerRoute } = workbox.routing;
 const { StaleWhileRevalidate } = workbox.strategies;
 
@@ -29,6 +30,6 @@ registerRoute(
 );
 
 // Ignore Urchin Tracking Module & Facebook's analytics tracking links
-precacheAndRoute(self.__WB_MANIFEST, {
+precacheAndRoute([{"revision":"31ed4268ccd1b0618af19cc7b11e7c7a","url":"index.html"},{"revision":"cfe08d5df75fd1d9aac63464bed927b4","url":"src/css/style.css"},{"revision":"5581d72e3c6f6500cea2f4b3cca1ef7b","url":"src/images/favicon.png"},{"revision":"9c4bbb21ac32475a2f3d8c55d2b7337d","url":"src/images/missing_cover.jpg"},{"revision":"87812cb2be86353aea34c4df8f4bce9c","url":"src/js/app.js"},{"revision":"e0802d1c11eb910c50b0df763dfbec32","url":"src/js/main.js"}], {
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
 });
